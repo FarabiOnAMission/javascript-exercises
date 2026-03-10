a95e859093c277fb754bee3ee120d1ce6216ba23
Farabi <86078777+FarabiOnAMission@users.noreply.github.com>
2026-03-10 13:11:41 +0600

    YEAAAAY FINISHED THE FINAL EXERCISE of FOUNDATIONS

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let year=-1;
+    let person={};
+    for(let i=0;i<arr.length;i++){
+        let birthYear = arr[i].yearOfBirth;
+        let deathYear = arr[i].yearOfDeath;
+        if(deathYear == undefined){
+            const d = new Date();
+            deathYear = d.getFullYear();
+        }
+        if(deathYear - birthYear >=year){
+            year = deathYear - birthYear;
+            person = arr[i];
+        }
+    }
 
+    return person;
 };
 
 // Do not edit below this line
